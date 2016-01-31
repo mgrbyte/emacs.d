9701003b0c04e6d2e2d05e50e0a9bea9ab403a7c
Matt Russell <matt@mgrbyte.co.uk>
2016-01-31 10:48:17 +0000

    Merge previously custom stuff from dot-files.

--- init.el
+++ init.el
@@ -7,27 +7,34 @@
 
 ;; Setup package management (Cask)
 (require 'cask "~/.cask/cask.el")
+
+(defun mgrbyte--add-to-hooks (function mode-hooks)
+  "Add FUNCTION to multiple modes MODE-HOOKS."
+  (mapc (lambda (hook) (add-hook hook function)) mode-hooks))
+
 (cask-initialize)
 (setq package-enable-at-startup nil)
 (package-initialize)
 
 (eval-when-compile
-  (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
-(require 'pallet)
-(require 'f)
-(require 's)
-
-;; Package configuration
+  (require 'use-package)
+  (require 'bind-key)
+  (require 'dash)
+  (require 'diminish)
+  (require 'f)
+  (require 'flycheck)
+  (require 'pallet)
+  (require 's))
 
 (use-package mgrbyte
+  :ensure abyss-theme
   :load-path "lisp"
-  :diminish mgrbyte-mode
+  :diminish 'mgrbyte-mode
   :config
   (add-hook 'after-init-hook #'mgrbyte-mode)
   ;; Turn off UI clutter
-  (mapc #'apply `((menu-bar-mode -1) (tool-bar-mode -1) (scroll-bar-mode -1)))
+  (mapc #'apply
+	`((menu-bar-mode -1) (tool-bar-mode -1) (scroll-bar-mode -1)))
 
   ;; Misc settings.
   (setq-default indent-line-function 'insert-tab)
@@ -63,30 +70,48 @@
   (put 'upcase-region 'disabled nil)
   (put 'downcase-region 'disabled nil)
   (put 'narrow-to-region 'disabled nil)
-  (put 'narrow-to-page 'disabled nil))
+  (put 'narrow-to-page 'disabled nil)
+  (bind-key "C-c t" #'tool-bar-mode)
 
-(use-package bookmark
-  :config
-  (define-key global-map [menu-bar bookmarks]
-    (cons "Bookmarks" (make-sparse-keymap "Bookmarks")))
-  (define-key global-map
-    [menu-bar bookmarks bookmark-insert]
-    '("Insert bookmark into buffer" . bookmark-insert))
-  (define-key global-map
-    [menu-bar bookmarks bookmark-delete]
-    '("Delete bookmark" . bookmark-delete))
-  (define-key global-map
-    [menu-bar bookmarks bookmark-save]
-    '("Save bookmarks" . bookmark-save))
-  (define-key global-map
-    [menu-bar bookmarks list-bookmarks]
-    '("List bookmarks" . list-bookmarks))
-  (define-key global-map
-    [menu-bar bookmarks bookmark-set]
-    '("Add bookmark" . bookmark-sebt))
-  (define-key global-map
-    [menu-bar bookmarks bookmark-jump]
-    '("Goto bookmark" . bookmark-jump)))
+  ;; Position
+  (setq initial-frame-alist
+	'((top . 0) (left . 200)
+	  (width . 320) (height . 10)))
+
+  ;; avoid audio beeping by turning on visible-bell
+  (setq visible-bell t)
+  (setq debug-on-error t)
+  (setq custom-theme-directory (locate-user-emacs-file "themes"))
+  (setq-default theme-load-from-file t)
+  (setq-default epg-gpg-program "gpg2")
+  (add-to-list 'auto-mode-alist '("Makfile.*" . makefile-gmake-mode))
+  (keyfreq-mode)
+  (menu-bar-mode 0)
+  (helm-mode 1)
+  (load-theme 'abyss t)
+
+  (use-package bookmark
+    :config
+    (define-key global-map [menu-bar bookmarks]
+      (cons "Bookmarks" (make-sparse-keymap "Bookmarks")))
+    (define-key global-map
+      [menu-bar bookmarks bookmark-insert]
+      '("Insert bookmark into buffer" . bookmark-insert))
+    (define-key global-map
+      [menu-bar bookmarks bookmark-delete]
+      '("Delete bookmark" . bookmark-delete))
+    (define-key global-map
+      [menu-bar bookmarks bookmark-save]
+      '("Save bookmarks" . bookmark-save))
+    (define-key global-map
+      [menu-bar bookmarks list-bookmarks]
+      '("List bookmarks" . list-bookmarks))
+    (define-key global-map
+      [menu-bar bookmarks bookmark-set]
+      '("Add bookmark" . bookmark-sebt))
+    (define-key global-map
+      [menu-bar bookmarks bookmark-jump]
+      '("Goto bookmark" . bookmark-jump))))
 
 (use-package browse-kill-ring
   :bind ("<kp-8>" . browse-kill-ring))
@@ -125,6 +150,8 @@
   :mode (("*scratch*" . emacs-lisp-mode)
 	 ("\\.el$" . emacs-lisp-mode)))
 
+(use-package erc)
+
 (use-package flycheck
   :bind ("<kp-7>" . flycheck-next-error)
   :preface
@@ -132,19 +159,62 @@
   (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
   (fringe-mode (quote (4 . 0)))
   (global-flycheck-mode 1)
+  (defun mgrbyte/flycheck-checker-name-on-mode-line (oldfun &optional status)
+    "Show the current checker name using OLDFUN and STATUS.
+
+Result will be shown in the flycheck mode-line."
+    (let ((res (apply oldfun status)))
+      ;; Unless there is no current checker
+      (if flycheck-checker
+	  (s-replace "FlyC" (format "FlyC[%s]" flycheck-checker) res)
+	res)))
   :config
-  (setq flycheck-emacs-lisp-load-path 'inherit)
-  (setq flycheck-python-flake8-executable "flake8")
-  (setq flycheck-flake8-maximum-line-length 79)
-  (setq flycheck-highlighting-mode 'lines))
+  (progn
+    (setq flycheck-emacs-lisp-load-path 'inherit)
+    (setq flycheck-python-flake8-executable "flake8")
+    (setq flycheck-flake8-maximum-line-length 79)
+    (setq flycheck-highlighting-mode 'lines)
+    (advice-add 'flycheck-select-checker
+		:after 'pyautomagic--remember-flycheck-checker)
+    (advice-add 'flycheck-mode-line-status-text
+		:around #'mgrbyte/flycheck-checker-name-on-mode-line)))
+
+;; Used for constributing 3rd party python packages
+;; instead of the more imposing flycheck-flake8 checker
+;;; (which is the default for my own and work packages)
+(use-package flycheck-pyflakes)
 
 (use-package flymake :disabled t)
+
+(use-package frame-cmds
+  :bind (("C-c f m" . maximize-frame)
+	 ("C-c f r" . restore-frame)
+	 ("C-c f o" . other-window-or-frame)
+	 ("<M-up>" . move-frame-up)
+	 ("<M-down>" . move-frame-down)
+	 ("<M-left>" . move-frame-left)
+ 	 ("<M-right>" . move-frame-right)))
 
 (use-package gist)
 
 (use-package git-gutter+)
 
+(use-package gnus
+  :bind (("C-x g" . gnus-other-frame)))
+
 (use-package google-this)
+
+(use-package ispell
+  :bind (("C-c i" . ispell-buffer))
+  :init
+  (mgrbyte--add-to-hooks
+   #'flyspell-mode `(LaTeX-mode-hook
+		     git-commit-mode-hook
+		     jabber-chat-mode-hook
+		     markdown-mode-hook
+		     message-mode-hook
+		     rst-mode-hook
+		     sphinx-doc-mode-hook)))
 
 (use-package js2-mode
   :mode (("\\.js$" . js2-mode))
@@ -153,6 +223,8 @@
 
 (use-package java-mode
   :mode (("\\.js.dtml$" . java-mode)))
+
+(use-package company-jedi)
 
 (use-package jedi
   :preface
@@ -163,10 +235,13 @@
 	 ("C-c r" . jedi:related-names)
 	 ("C-?" . jedi:show-doc)))
 
+(use-package keyfreq)
+
 (use-package ls-lisp
   :config (setq ls-lisp-use-insert-directory-program nil))
 
-(use-package magit)
+(use-package magit
+  :bind (("C-c m" . magit-status)))
 
 (use-package mardown-mode
   :mode (("\\.md$" . markdown-mode)))
@@ -180,6 +255,9 @@
   (set-selection-coding-system 'utf-8)
   (prefer-coding-system 'utf-8))
 
+(use-package notify
+  :load-path "lisp")
+
 (use-package nxml-mode
   :mode (("\\.xml$" . nxml-mode)
          ("\\.zcml$" . nxml-mode))
@@ -188,9 +266,33 @@
 	    (lambda ()
 	      (setq indent-tabs-mode nil))))
 
+(use-package paredit
+  :diminish paredit-mode
+  :config
+  (mgrbyte--add-to-hooks
+   #'enable-paredit-mode `(lisp-mode-hook emacs-lisp-mode-hook)))
+
 (use-package paren
-  :config (setq show-paren-style 'expression)
-  :init (show-paren-mode 1))
+  :config
+  (setq show-paren-style 'expression)
+  :init
+  (show-paren-mode 1))
+
+(use-package powerline
+  :config
+  (setq-default powerline-default-separator 'wave))
+
+(use-package pretty-symbols
+  :diminish pretty-symbols-mode
+  :preface
+  (defun enable-pretty-symbols-mode ()
+    (pretty-symbols-mode 1))
+  :config
+  (mgrbyte--add-to-hooks
+   #'enable-pretty-symbols-mode `(emacs-lisp-mode-hook
+				  lisp-mode-hook
+				  clojure-mode-hook
+				  python-mode-hook)))
 
 (use-package python
   :bind (("<kp-5>" . py-insert-debug)
@@ -206,7 +308,16 @@
   (setq tab-width 4)
   (sphinx-doc-mode t))
 
+(use-package pyautomagic
+  :load-path "lisp"
+  :bind (("C-c v e" . pyautomagic--activate-venv-safely)
+	 ("C-c f c" . pyautomagic--configure-flycheck-checkers)))
+
 (use-package pyvenv)
+
+(use-package rainbow-delimiters
+  :config
+  (rainbow-delimiters-mode-enable))
 
 (use-package rst
   :config
@@ -267,10 +378,18 @@
   :mode (("\\.po$" . text-mode)
 	 ("\\.pot$" . text-mode)))
 
+(use-package text-scale-mode
+  :bind (("C-c +" . text-scale-increase)
+	 ("C-c -" . text-scale-decrease)))
+
 (use-package vc)
 
 (use-package vcl
   :mode (("\\.vcl" . vcl-mode)))
+
+(use-package yaml-mode
+  :mode (("\\.yml" . yaml-mode)
+	 ("\\.yaml" . yaml-mode)))
 
 ;; Ensure PATH is preserved from shell.
 (exec-path-from-shell-initialize)
@@ -282,6 +401,7 @@
     (insert-file-contents
      (locate-user-emacs-file "user-custom-file-template.el") nil 0)
      (write-region (buffer-string) nil user-custom-file)))
+
 (load user-custom-file)
 
 ;; Code generated by using the Emacs *cusomize* interfaces goes to its own file.
