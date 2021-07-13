39cac7f7e8524b6c080fc7ca2bd447273258e67b
Matt Russell <matt@clojurian.me.uk>
2021-07-13 14:48:38 +0100

    Handle not working with X display.

--- init.el
+++ init.el
@@ -248,13 +248,15 @@ https://glyph.twistedmatrix.com/2015/11/editor-malware.html"
   :config
   ;; Track changes to install packages with Cask
   (pallet-mode t)
-  ;; Turn off UI elements
-  (mapc #'apply
-	`((menu-bar-mode -1) (tool-bar-mode -1) (scroll-bar-mode -1)))
-  ;; setup frame
-  (if (daemonp)
-      (add-hook 'after-make-frame-functions #'mgrbyte-setup-frame)
-    (mgrbyte-setup-frame))
+
+  (when (getenv "DISPLAY")
+    ;; Turn off UI elements
+    (mapc #'apply
+	  `((menu-bar-mode -1) (tool-bar-mode -1) (scroll-bar-mode -1)))
+    ;; setup frame
+    (if (daemonp)
+	(add-hook 'after-make-frame-functions #'mgrbyte-setup-frame)
+      (mgrbyte-setup-frame)))
 
   ;; Misc settings.
   (setq-default indent-line-function 'insert-tab)
