1debf141ca12eff4249d501ff9b9e2eb6160e420
Matt Russell <m.russell@bangor.ac.uk>
2026-01-06 17:18:31 +0000

    Fix nerd fonts in daemon mode

--- lisp/init-icons.el
+++ lisp/init-icons.el
@@ -10,10 +10,15 @@
   (nerd-icons-font-family "Symbols Nerd Font Mono")
   :config
   ;; Make Emacs use nerd font for symbol ranges (PUA where nerd icons live)
-  (when (display-graphic-p)
+  (defun mgrbyte-setup-nerd-fonts ()
+    "Configure fontset to use nerd fonts for icon codepoints."
     (set-fontset-font t 'symbol "Symbols Nerd Font Mono" nil 'prepend)
     (set-fontset-font t '(#xe000 . #xf8ff) "Symbols Nerd Font Mono" nil 'prepend)
-    (set-fontset-font t '(#xf0000 . #xfffff) "Symbols Nerd Font Mono" nil 'prepend)))
+    (set-fontset-font t '(#xf0000 . #xfffff) "Symbols Nerd Font Mono" nil 'prepend))
+  ;; Run when GUI is available (handles daemon mode)
+  (if (display-graphic-p)
+      (mgrbyte-setup-nerd-fonts)
+    (add-hook 'server-after-make-frame-hook #'mgrbyte-setup-nerd-fonts)))
 
 (use-package nerd-icons-dired
   :ensure t
