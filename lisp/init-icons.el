57338e20e50b0954e9e457b4455b3d492718e388
Matt Russell <m.russell@bangor.ac.uk>
2026-01-06 16:49:52 +0000

    Update nerd icon config.

--- lisp/init-icons.el
+++ lisp/init-icons.el
@@ -7,7 +7,13 @@
   :ensure t
   :demand t
   :custom
-  (nerd-icons-font-family "Symbols Nerd Font Mono"))
+  (nerd-icons-font-family "Symbols Nerd Font Mono")
+  :config
+  ;; Make Emacs use nerd font for symbol ranges (PUA where nerd icons live)
+  (when (display-graphic-p)
+    (set-fontset-font t 'symbol "Symbols Nerd Font Mono" nil 'prepend)
+    (set-fontset-font t '(#xe000 . #xf8ff) "Symbols Nerd Font Mono" nil 'prepend)
+    (set-fontset-font t '(#xf0000 . #xfffff) "Symbols Nerd Font Mono" nil 'prepend)))
 
 (use-package nerd-icons-dired
   :ensure t
