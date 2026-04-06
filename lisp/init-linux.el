e5cf9f93d5e04cb088e43fd5b66e9a3e37cae091
Matt Russell <m.russell@bangor.ac.uk>
2026-04-06 15:39:35 +0100

    Hack to make edwim work in magit on Linux

--- lisp/init-linux.el
+++ lisp/init-linux.el
@@ -6,7 +6,18 @@
 ;; HHKB cmd key sends Super (Mod4) on Linux. Interpret Super as Meta in Emacs.
 ;; GNOME Shell intercepts Super independently, so it is unaffected.
 (when (eq system-type 'gnu/linux)
-  (setq x-meta-keysym 'super))
+  (setq x-meta-keysym 'super)
+
+  ;; Emacs 30 daemon mode: make-frame no longer reliably inherits the display
+  ;; from the current frame, falling back to the daemon's TTY and crashing.
+  ;; Explicitly pass the current frame's display to ediff's control frame.
+  (with-eval-after-load 'ediff-wind
+    (advice-add 'ediff-setup-control-frame :around
+                (lambda (orig-fun &rest args)
+                  (let ((ediff-control-frame-parameters
+                         (cons (cons 'display (frame-parameter (selected-frame) 'display))
+                               ediff-control-frame-parameters)))
+                    (apply orig-fun args))))))
 
 (provide 'init-linux)
 ;;; init-linux.el ends here
