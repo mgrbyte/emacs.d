93b67ce4b858ddeffe230cc3561fd978d0c4b2e9
Matt Russell <m.russell@bangor.ac.uk>
2026-04-09 23:22:11 +0100

    Set treemacs-text-scale to -1 for better readability

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -3,29 +3,13 @@
 ;; Setup treemacs file explorer
 ;;; Code:
 
-(defun mgrbyte-treemacs-fit-width ()
-  "Resize the treemacs window to fit the longest visible line plus a small margin."
-  (when-let* ((buf (treemacs-get-local-buffer))
-              (win (get-buffer-window buf t)))
-    (with-current-buffer buf
-      (let ((max-len 0))
-        (save-excursion
-          (goto-char (point-min))
-          (while (not (eobp))
-            (end-of-line)
-            (setq max-len (max max-len (current-column)))
-            (forward-line 1)))
-        (let ((new-width (+ max-len 2)))
-          (unless (= new-width (window-width win))
-            (setq treemacs-width new-width)
-            (window-resize win (- new-width (window-width win)) t)))))))
-
 (use-package treemacs
 
   :bind (("C-c e" . treemacs)
          ("C-c E" . treemacs-select-window))
   :config
   (setq treemacs-width 35)
+  (setq treemacs-text-scale -1)
   (setq treemacs-position 'right)
   (setq treemacs-project-follow-cleanup t)
   ;; Hide gitignored files
@@ -46,10 +30,7 @@
           ".ruff_cache"
           ".pytest_cache"
           "*.egg-info"))
-  (treemacs-project-follow-mode t)
-  (add-hook 'treemacs-post-project-refresh-functions #'mgrbyte-treemacs-fit-width)
-  (add-hook 'treemacs-after-node-open-close-functions
-            (lambda (_) (mgrbyte-treemacs-fit-width))))
+  (treemacs-project-follow-mode t))
 
 (use-package treemacs-projectile
 
