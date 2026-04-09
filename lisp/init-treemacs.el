7ccd0d7a8f2d28bc62e2e6650ef9db1b8d53eef6
Matt Russell <m.russell@bangor.ac.uk>
2026-04-09 21:31:09 +0100

    Resize treemacs window dynamically to fit longest visible path
    - Add mgrbyte-treemacs-fit-width, hooked into
      treemacs-post-project-refresh-functions and
      treemacs-after-node-open-close-functions

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -3,6 +3,23 @@
 ;; Setup treemacs file explorer
 ;;; Code:
 
+(defun mgrbyte-treemacs-fit-width ()
+  "Resize the treemacs window to fit the longest visible line plus a small margin."
+  (when-let* ((buf (treemacs-get-local-buffer))
+              (win (get-buffer-window buf t)))
+    (with-current-buffer buf
+      (let ((max-len 0))
+        (save-excursion
+          (goto-char (point-min))
+          (while (not (eobp))
+            (end-of-line)
+            (setq max-len (max max-len (current-column)))
+            (forward-line 1)))
+        (let ((new-width (+ max-len 2)))
+          (unless (= new-width (window-width win))
+            (setq treemacs-width new-width)
+            (window-resize win (- new-width (window-width win)) t)))))))
+
 (use-package treemacs
 
   :bind (("C-c e" . treemacs)
@@ -29,7 +46,10 @@
           ".ruff_cache"
           ".pytest_cache"
           "*.egg-info"))
-  (treemacs-project-follow-mode t))
+  (treemacs-project-follow-mode t)
+  (add-hook 'treemacs-post-project-refresh-functions #'mgrbyte-treemacs-fit-width)
+  (add-hook 'treemacs-after-node-open-close-functions
+            (lambda (_) (mgrbyte-treemacs-fit-width))))
 
 (use-package treemacs-projectile
 
