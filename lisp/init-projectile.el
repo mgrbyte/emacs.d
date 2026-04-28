614f95c47bed1a59c9d53c3d98b8972243e1f4e6
Matt Russell <mgrbyte@member.fsf.org>
2026-04-28 15:54:36 +0100

    Prevent TRAMP round-trips when opening remote projects from dashboard

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -16,7 +16,15 @@
   (setq projectile-project-search-path '(("~/github/mgrbyte" . 1)
                                          ("~/gitlab/mtr21pqh" . 2)
                                          ("~/gitlab/cyfieithu-ac-llms" . 3)
-                                         ("~/huggingface/spaces" . 2))))
+                                         ("~/huggingface/spaces" . 2)))
+
+  ;; Recognise known projects immediately without filesystem access.
+  ;; Prevents TRAMP round-trips when opening remote projects from the dashboard.
+  (defun mgrbyte-projectile-root-known (dir &optional _list)
+    "Return known project root matching DIR without filesystem access."
+    (cl-find-if (lambda (known) (string-prefix-p known dir))
+                projectile-known-projects))
+  (add-to-list 'projectile-project-root-functions #'mgrbyte-projectile-root-known))
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here
